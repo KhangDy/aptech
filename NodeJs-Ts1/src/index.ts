21831ffb488cda2fa9eb09a41d45f1fb7a751642
Dỹ Trương <khangdy38@gmail.com>
2024-01-12 13:45:17 +0700

    12/1

--- NodeJs-Ts1/src/index.ts
+++ NodeJs-Ts1/src/index.ts
@@ -1,5 +1,30 @@
-type Handle = () => Promise<string>
-const fullname = 'Dư Thanh Được'
-const person: { name: string } = { name: fullname }
-const handle: Handle = () => Promise.resolve(fullname)
-handle().then(console.log)
+import express from 'express'
+const app = express()
+const router = express.Router()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+router.use ((req, res, next) => {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+router.get('/tweets', (req, res) => {
+  res.json({
+    data : [
+      {
+        id: 1,
+        text: "hi world"
+      }
+    ]
+  })
+})
+
+app.use('/api', router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
